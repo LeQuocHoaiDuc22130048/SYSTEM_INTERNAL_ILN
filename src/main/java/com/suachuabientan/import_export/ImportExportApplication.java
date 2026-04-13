446d19008c88a4fcaf348722a6adf86397272f48
HoaiDuc <lequochoaiduc04@gmail.com>
2026-04-13 17:16:01 +0700

    setup environment for project

---
+++ src/main/java/com/suachuabientan/import_export/ImportExportApplication.java
@@ -0,0 +1,13 @@
+package com.suachuabientan.import_export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportExportApplication.class, args);
+	}
+
+}
